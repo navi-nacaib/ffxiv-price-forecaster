2d4ea858e0c568a578910764e7eaf090f2992e14
Ivan Biacan <ivan.biacan@gmail.com>
2024-11-02 16:52:59 -0700

    more changes

--- dockerImageToECR.bat
+++ dockerImageToECR.bat
@@ -2,10 +2,10 @@
 aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 218821328974.dkr.ecr.us-west-1.amazonaws.com
 
 :: Build the Docker image
-docker build -t ffxiv-price-forecaster .
+docker build -t serverless-ffxiv-price-forecaster .
 
 :: Tag the Docker image
-docker tag ffxiv-price-forecaster:latest 218821328974.dkr.ecr.us-west-1.amazonaws.com/ffxiv-price-forecaster:latest
+docker tag serverless-ffxiv-price-forecaster:latest 218821328974.dkr.ecr.us-west-1.amazonaws.com/serverless-ffxiv-price-forecaster:latest
 
 :: Push the Docker image to ECR
-docker push 218821328974.dkr.ecr.us-west-1.amazonaws.com/ffxiv-price-forecaster:latest
+docker push 218821328974.dkr.ecr.us-west-1.amazonaws.com/serverless-ffxiv-price-forecaster:latest
